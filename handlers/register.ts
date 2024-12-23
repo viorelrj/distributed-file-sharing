9b1c8be2ac5f0e4db99278f5fb098a83b2bb1b8f
Viorel Rînja <viorel.rinja@gmail.com>
2024-12-23 18:24:09 +0200

    cleanup

--- handlers/register.ts
+++ handlers/register.ts
@@ -1,12 +1,6 @@
 import { REGISTER, UPLOAD } from "@/events.ts";
 import { createHandler } from "@/utils/createHandler.ts";
 
-type payload = string;
-type REGISTER = {
-  type: 'register',
-  payload: payload
-}
-
 const withRegister = createHandler(REGISTER, (socket, payload) => {
   console.log(payload.id);
   socket.emit(UPLOAD, 1);
